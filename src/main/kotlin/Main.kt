08a92e1ac70ed4a9b7113ccb698bf2f8d8af67f3
Estela Rubio <estela.rubio@colba.es>
2024-01-23 15:36:21 +0100

    My solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,16 +12,25 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+@Composable // Tienen que tenerlo porque el compilador tiene que pasar por ellos para añadir el codigo necesario
+@Preview // Permite previsualizar la app si tienes el plugin "Compose Multiplatform"
+fun App() { // Representa a la aplicación
+    // mutableStateOf --> así es como se crea un estado
+    // la función remember hace que el estado se acuerde que se ha modificado
+    val text = remember { mutableStateOf("") }
+    var inputText = ""
+    MaterialTheme {  // Material es reactivo al estado, cada vez que se modifica un estado, material se actualiza
+        Column {
+            TextField(value = inputText, onValueChange = {newText ->
+                inputText = newText
+                text.value = inputText})
+            Text(text = "Hello ${text.value}")
+            Button(onClick = {
+                text.value = ""
+                inputText = ""
+            }) {
+                Text("Clean")
+            }
         }
     }
 }
