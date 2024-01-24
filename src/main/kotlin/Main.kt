2c6f91d1c719d1766a548dfff92266d3702ef3ba
Estela Rubio <estela.rubio@colba.es>
2024-01-24 17:19:11 +0100

    Our solution data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
