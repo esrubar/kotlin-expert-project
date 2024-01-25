0776bbdbf6c8cc7b806a658854041f1699e4d63f
Estela Rubio <estela.rubio@colba.es>
2024-01-25 13:51:30 +0100

    Lambdas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,53 +3,76 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlin.concurrent.thread
 
 class AppState {
-    val notes = mutableStateOf(getNotes())
+    val state = mutableStateOf(UiState())
+    fun loadNotes() {
+        thread {
+            state.value = UiState(loading = true)
+            getNotes { state.value = UiState(notes = it, loading = false) }
+        }
+    }
+    data class UiState(
+        val notes: List<Note> = emptyList(),
+        val loading: Boolean = false
+    )
 }
 
 @Composable
 @Preview
 fun App(appState: AppState) {
-    MaterialTheme {
-       LazyColumn (
-           modifier = Modifier.fillMaxWidth(),
-           horizontalAlignment = Alignment.CenterHorizontally
-       ){
-           items(appState.notes.value) { note ->
-               Card(
-                   modifier = Modifier.padding(8.dp).fillMaxWidth()
-               ) {
-                   Column(modifier = Modifier.padding(16.dp)) {
-                       Row {
-                           Text(text = note.title, style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
-                           if(note.type == Note.Type.AUDIO) {
-                               Icon(imageVector = Icons.Default.Mic, contentDescription = null)
-                           }
-                       }
 
-                       Spacer(modifier = Modifier.height(8.dp))
-                       Text(text = note.description)
-                   }
-               }
-           }
-       }
+    LaunchedEffect(true) {
+        appState.loadNotes()
+    }
+
+    MaterialTheme {
+        Box(contentAlignment = Alignment.Center) {
+            if(appState.state.value.loading) {
+                CircularProgressIndicator()
+            }
+            NotesList(appState.state.value.notes)
+        }
     }
 }
-fun buildMessage(value: String) = "Hello $value"
+
+@Composable
+private fun NotesList(notes: List<Note>) {
+    LazyColumn (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        items(notes) { note ->
+            Card(
+                modifier = Modifier.padding(8.dp).fillMaxWidth()
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Row {
+                        Text(text = note.title, style = MaterialTheme.typography.h6, modifier = Modifier.weight(1f))
+                        if(note.type == Note.Type.AUDIO) {
+                            Icon(imageVector = Icons.Default.Mic, contentDescription = null)
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = note.description)
+                }
+            }
+        }
+    }
+}
 
 fun main() {
     // Lo que creemos aquí va a vivir toda la vida de la aplicación
--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -4,12 +4,16 @@ data class Note(val title: String, val description: String = "", val type: Type 
     }
 }
 
-fun getNotes(): List<Note> = (1..10).map {
-    Note(
-        "Title $it",
-        "Description $it",
-        if (it % 3 == 0) Note.Type.AUDIO else Note.Type.TEXT
-    )
+fun getNotes(callback: (List<Note>) -> Unit) {
+    Thread.sleep(2000)
+    val notes = (1..10).map {
+        Note(
+            "Title $it",
+            "Description $it",
+            if (it % 3 == 0) Note.Type.AUDIO else Note.Type.TEXT
+        )
+    }
+    callback(notes)
 }
 
 fun createNote() {
