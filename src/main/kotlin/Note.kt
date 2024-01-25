957575607cd673dcd85c6f8244051247907c6c15
Estela Rubio <estela.rubio@colba.es>
2024-01-25 13:11:15 +0100

    2.13 UI del listado de notas

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,4 +1,4 @@
-data class Note(val title: String, val description: String, val type: Type) {
+data class Note(val title: String, val description: String = "", val type: Type = Type.TEXT) {
     enum class Type {
         AUDIO, TEXT
     }
@@ -10,4 +10,8 @@ fun getNotes(): List<Note> = (1..10).map {
         "Description $it",
         if (it % 3 == 0) Note.Type.AUDIO else Note.Type.TEXT
     )
+}
+
+fun createNote() {
+    Note(title = "Titulo 1", type = Note.Type.AUDIO)
 }
