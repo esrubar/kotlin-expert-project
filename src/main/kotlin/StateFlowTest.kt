aabbc85f4882451f3daa2ee8542187527e3dcb3b
Estela Rubio <estela.rubio@colba.es>
2024-01-29 10:56:00 +0100

    StateFlow

---
+++ src/main/kotlin/StateFlowTest.kt
@@ -0,0 +1,32 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// Stateflow
+// Se utiliza para almacenar un estado y que ese estado se pueda consumir de forma reactiva. Es interesante cuando ese
+// estado representa la interfaz
+class ViewModel {
+    // Backing property. Para que no se pueda settear desde fuera
+    private var _state: MutableStateFlow<Note> = MutableStateFlow(Note("title 1", "description 1", Note.Type.TEXT))
+    val state: StateFlow<Note> = _state.asStateFlow()
+
+    suspend fun update() {
+        var count = 1
+        while (true) {
+            delay(2000)
+            count++
+            _state.value = Note("title $count", "description $count", Note.Type.TEXT)
+        }
+    }
+}
+
+fun main(): Unit = runBlocking {
+    val viewModel = ViewModel()
+    launch {
+        viewModel.update()
+    }
+    viewModel.state.collect(::println)
+}
