e2c3b9e689d13a4ac8bfaeca286b9a74f525fb6f
Estela Rubio <estela.rubio@colba.es>
2024-01-30 14:05:08 +0100

    Operator overloading

--- src/main/kotlin/com/devexperto/kotlinexpert/data/Note.kt
+++ src/main/kotlin/com/devexperto/kotlinexpert/data/Note.kt
@@ -10,6 +10,10 @@ data class Note(val title: String, val description: String = "", val type: Type 
     }
 }
 
+operator fun Note.plus(other: Note): Note = Note(title, "$description &(other.description)", type)
+fun test(note1: Note, note2: Note) = note1 + note2
+fun test2(notes: List<Note>, note: Note) = notes + note
+
 fun getNotes(): Flow<List<Note>> = flow {
     delay(200)
     val notes = (0..10).map {
